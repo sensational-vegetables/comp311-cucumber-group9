b06820d21bab701c096d692bfa0399f0ac73cad0
sensational-vegetables <sensational-vegetables@outlook.com>
2024-07-27 12:06:14 +0000

    Pending changes exported from your codespace

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# comp311-cucumber-group9
+# COMP311 Cucumber Group9
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/sensational-vegetables/comp311-cucumber-group9)
+
+Please see: https://cucumber.io/docs/guides/10-minute-tutorial/?lang=java
---
+++ src/test/java/testrunner/TestRunner.java
@@ -0,0 +1,11 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/hellocucumber", glue = "hellocucumber", plugin = { "pretty",
+                "html:target/cucumber-reports.html" })
+public class TestRunner {
+}
